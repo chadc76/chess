90a8703d0b268afbd2b4e5e1b462734750308227
Chad Cunningham <chadcunningham7676@gmail.com>
2020-10-10 22:19:36 -0400

    Added move_toward_upgrade? and can_upgrade? methods for moves

--- computer.rb
+++ computer.rb
@@ -10,10 +10,14 @@ class ComputerPlayer < Player
       move = piece_threatened?(_board)    
     elsif capture_piece(_board)
       move = capture_piece(_board)
+    elsif can_upgrade?(_board)
+      move = can_upgrade?(_board)
     elsif safe_check(_board) && in_endgame?(_board)
       move = safe_check(_board)
     elsif check_possible(_board) && in_endgame?(_board)
       move = check_possible(_board)
+    elsif move_toward_upgrade?(_board) && (_board.pieces.select{|p| p.color == color && p.class != Pawn}).count < 4
+      move = move_toward_upgrade?(_board)
     else
       move = random_move(_board)
     end
@@ -61,13 +65,13 @@ class ComputerPlayer < Player
       copy = _board.dup
       copy.move_piece!(piece1.pos, piece2.pos)
       opponent_color = piece2.color
-      opponent_moves(opponent_color, copy).none?{|move| move == piece2.pos }
+      opponent_moves(copy).none?{|move| move == piece2.pos }
     else
       true
     end
   end
 
-  def opponent_moves(opponent_color, _board)
+  def opponent_moves(_board)
     opponent_pieces = _board.pieces.select{|p| p.color == opponent_color}
     opponent_moves = []
     opponent_pieces.each do |piece|
@@ -79,7 +83,7 @@ class ComputerPlayer < Player
   def piece_threatened?(_board)
     strong_pieces = _board.pieces.select{|p| p.color == color && p.class != Pawn}
     copy = _board.dup
-    opponents = opponent_moves(opponent_color, copy)
+    opponents = opponent_moves(copy)
     threatened = threatened_pieces(strong_pieces, opponents)
     return false if threatened.empty?
     move = value_moves(threatened, _board)
@@ -106,15 +110,15 @@ class ComputerPlayer < Player
         start_pos = piece.pos
       end
     end
-    move = safe_move(_board[start_pos], _board)
+    move = safe_moves(_board[start_pos], _board)
     move
   end
 
-  def safe_move(piece, _board)
+  def safe_moves(piece, _board)
     piece.valid_moves.each do |end_pos|
       copy = _board.dup
       copy.move_piece!(piece.pos, end_pos)
-      return [piece.pos, end_pos] if opponent_moves(opponent_color, copy).none?{|move| move == end_pos}
+      return [piece.pos, end_pos] if opponent_moves(copy).none?{|move| move == end_pos}
     end
     return []
   end
@@ -124,7 +128,7 @@ class ComputerPlayer < Player
       piece.valid_moves.each do |end_pos|
       copy = _board.dup
       copy.move_piece!(piece.pos, end_pos)
-      return [piece.pos, end_pos] if copy.in_check?(opponent_color) && opponent_moves(opponent_color, copy).none?{|move| move == end_pos}
+      return [piece.pos, end_pos] if copy.in_check?(opponent_color) && opponent_moves(copy).none?{|move| move == end_pos}
       end
     end
     false
@@ -159,5 +163,53 @@ class ComputerPlayer < Player
   def in_endgame?(_board)
     strong_pieces = _board.pieces.select{|p| p.color == opponent_color && p.class != Pawn}
     strong_pieces.none?{|p| p.class == Queen} && strong_pieces.count <= 4
+  end
+
+  def move_toward_upgrade?(_board)
+    opponent_move = opponent_moves(_board)
+    opponents_pos = _board.pieces.select{|p| p.color == opponent_move}.map(&:pos)
+    move = []
+    pawns(_board).each do |pawn|
+      path = pawn_path(pawn.pos)
+      valid_path =  path.all? {|end_pos| !opponents_pos.include?(end_pos) && !opponent_move.include?(end_pos) }
+      next if !valid_path
+      move = pawn.valid_moves.include?(path[1]) ? [pawn.pos, path[1]] : [pawn.pos, path[0]]
+      return move
+    end
+    false
+  end
+
+  def pawn_path(pos)
+    target = color == :white ? 0 : 7
+    change = target == 0 ? -1 : 1
+    r,c = pos
+    path = []
+    until r == target + change
+      r += change
+      path << [r, c]
+    end
+    path
+  end
+
+  def can_upgrade?(_board)
+    target = color == :white ? 0 : 7
+    pawns = pawns(_board)
+    pawns.each do |pawn|
+      pawn.valid_moves.each do |end_pos|
+        return [pawn.pos, end_pos] if end_pos[0] == target && safe_move(pawn, end_pos, _board)
+      end
+    end
+    false
+  end
+
+  def pawns(_board)
+    pieces(_board).select{|p| p.class == Pawn && p.color == color}
+  end
+
+  def safe_move(piece, end_pos, _board)
+    copy = _board.dup
+    copy.move_piece!(piece.pos, end_pos)
+    opponent_move = opponent_moves(copy)
+    !opponent_move.include?(end_pos)
   end
 end
